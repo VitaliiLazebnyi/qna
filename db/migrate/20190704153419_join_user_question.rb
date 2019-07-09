7187a58bb2914e0284de6ace7fbfedcb3d82ba39
Vitalii Lazebnyi <vitalii.lazebnyi@gmail.com>
2019-07-09 11:13:40 +0300

    create db references in proper way

--- db/migrate/20190704153419_join_user_question.rb
+++ db/migrate/20190704153419_join_user_question.rb
@@ -1,5 +1,5 @@
 class JoinUserQuestion < ActiveRecord::Migration[5.2]
   def change
-    add_reference :questions, :user, index: true
+    add_reference :questions, :user, index: true, foreign_key: true
   end
 end
