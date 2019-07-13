82fd9db9617b3dc01d30b592b02c8e7134f18536
Vitalii Lazebnyi <vitalii.lazebnyi@gmail.com>
2019-07-14 02:21:37 +0300

    add answer with ajax

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,13 +7,13 @@ class AnswersController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    @answer   = @question.answers.new(answer_params.merge(user: current_user))
+    @answer   = @question.answers.create(answer_params.merge(user: current_user))
 
-    if @answer.save
-      redirect_to @question, notice: 'Your answer was successfully created.'
-    else
-      render 'questions/show'
-    end
+    # if @answer.save
+    #   redirect_to @question, notice: 'Your answer was successfully created.'
+    # else
+    #   render 'questions/show'
+    # end
   end
 
   def destroy
