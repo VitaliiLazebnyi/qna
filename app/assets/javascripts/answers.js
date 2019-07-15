68f7aea7e7275abce3465c74cb941d5ae1642035
Vitalii Lazebnyi <vitalii.lazebnyi@gmail.com>
2019-07-15 23:55:25 +0300

    video done

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,12 +1,16 @@
-$(document).ready(
-  function(){
-      $('.edit-answer-link').on(
-          'click', function(e){
-              e.preventDefault();
-              $(this).hide();
-              let answerId = $(this).data('answerId');
-              $('form#edit-answer-' + answerId).show();
-          }
-      )
-  }
+function setEventOnEditAnsweLink(){
+    $('.answers').on(
+        'click',
+        '.edit-answer-link',
+        function(e){
+            e.preventDefault();
+            $(this).hide();
+            let answerId = $(this).data('answerId');
+            $('form#edit-answer-' + answerId).removeClass('hidden');
+        }
+    );
+}
+
+$(document).on("turbolinks:load",
+    setEventOnEditAnsweLink
 );
