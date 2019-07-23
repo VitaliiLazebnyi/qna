fb31dec71c05a87d8422070975e2d5787efad0f7
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2019-07-24 00:11:13 +0300

    multiple files

--- spec/features/questions_create_spec.rb
+++ spec/features/questions_create_spec.rb
@@ -42,11 +42,12 @@ feature 'User can create questions', '
     click_on 'Ask question'
     fill_in 'Title', with: question.title
     fill_in 'Body', with: question.body
-    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    attach_file 'Files', %W(#{Rails.root}/spec/rails_helper.rb #{Rails.root}/spec/spec_helper.rb)
 
     click_on 'create'
 
     expect(page).to have_link 'rails_helper.rb'
+    expect(page).to have_link 'spec_helper.rb'
   end
 
   scenario 'unauthenticated user creates a question' do
