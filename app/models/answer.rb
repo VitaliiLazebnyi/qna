67667eb7a6cb06114d0dd39bb00848ac2ce677c5
Vitalii Lazebnyi <vitalii.lazebnyi@gmail.com>
2019-08-13 18:50:26 +0300

    simplify

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -16,7 +16,7 @@ class Answer < ApplicationRecord
   def make_best!
     transaction do
       question.answers.where.not(id: id).update_all(best: false)
-      question.award&.update!(user_id: user.id)
+      question.award&.update!(user: user)
       update!(best: true)
     end
   end
